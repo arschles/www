83f89807c3555dad37c08bf0fdeb44fcd43748d5
Aaron Schlesinger <aaron@ecomaz.net>
2021-10-06 03:28:47 +0000

    creating redirect server
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- wrk/src/handler.ts
+++ wrk/src/handler.ts
@@ -1,3 +1,27 @@
-export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+import {Router, Request, Obj} from 'itty-router'
+// import * as redirects from '.'
+
+declare const redirects: KVNamespace;
+
+export function getRouter() : Router<void> {
+  const router = Router()
+  router.get('/*', async(req: Request): Promise<Response> => {
+    try {
+      const url = new URL(req.url)
+      const path = url.pathname.slice(1)
+      const redir_val = await redirects.get(path)
+      if(redir_val == null) {
+        return error(500, `no redirect for /${path}`)
+      }
+      return Response.redirect(redir_val)
+    } catch(e: any) {
+      return error(500, `exception thrown: ${e}`)
+    }
+  })
+
+  return router
+}
+
+function error(code: number, body: string): Response {
+  return new Response(body, {status: code});
 }
